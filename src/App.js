e97de770d153d1576c7e188ae2828615ba636777
Jesse-L2 <jesse.little@utexas.edu>
2024-02-07 12:45:54 -0600

    Add Home route

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
       </Routes>
     </>
-  );
+  )
 }
 
 export default App;
