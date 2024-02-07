3abf8bcd6d008f50450f0dac259bd9c05c42df5b
Jesse-L2 <jesse.little@utexas.edu>
2024-02-07 15:22:16 -0600

    Update home route

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,18 @@
-const Home = () => 'Hello';
+import LogoTitle from '../../assets/images/logo-s.png';
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone"></div>
+      <h1>
+        Hi, <br />
+        <img src={LogoTitle} alt="developer"> how are you?
+      </h1>
+      <h2>Fullstack Web Developer</h2>
+      <Link to="/contact" className="flat-button">Contact Me</Link>
+
+    </div>
+  );
+};
 
 export default Home;
